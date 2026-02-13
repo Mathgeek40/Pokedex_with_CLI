126c94cbd575044868cd9f159a53d03eb565057e
Mathgeek40 <28siemensi@tbs.edu.np>
2026-02-13 08:47:11 +0545

    Update and rename Testing.py to setup.py

--- Testing.py
+++
@@ -1,2 +0,0 @@
-h = [str(i) for i in range(10)]
-print(", ".join(h))
---
+++ setup.py
@@ -0,0 +1,8 @@
+try:
+  pip3 install io
+  pip3 install os 
+  pip3 install time
+except:
+  pip install io
+  pip install os 
+  pip install time
